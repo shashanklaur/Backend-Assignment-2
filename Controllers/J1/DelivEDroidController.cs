fbe2e12246ad9aa7534e3ec6b92eb094a79c3cbd
Shashank Laur <shashanklaur@gmail.com>
2024-10-31 20:39:04 -0400

    Add files via upload

---
+++ Controllers/J1/DelivEDroidController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C_Assignment_2.Controllers.J1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DelivEDroidController : ControllerBase
+    {
+        /// <summary>
+        /// Calculates the final score based on the deliveries and collisions.
+        /// </summary>
+        /// <param name="deliveries">The number of packages delivered</param>
+        /// <param name="collisions">The number of collisions with obstacles</param>
+        /// <returns>The final score</returns>
+        /// <example>POST: /api/DelivEDroid?deliveries=5&collisions=2</example>
+        [HttpPost]
+        public IActionResult CalculateScore([FromForm] int deliveries, [FromForm] int collisions)
+        {
+            int score = (deliveries * 50) - (collisions * 10);
+            if (deliveries > collisions)
+            {
+                score += 500; // bonus points
+            }
+            return Ok(score);
+        }
+    }
+}
---
+++ Controllers/J2/ChiliPeppersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C_Assignment_2.Controllers.J2
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChiliPeppersController : ControllerBase
+    {
+        private readonly Dictionary<string, int> pepperHeatUnits = new Dictionary<string, int>
+        {
+            { "Poblano", 1500 },
+            { "Mirasol", 6000 },
+            { "Serrano", 15500 },
+            { "Cayenne", 40000 },
+            { "Thai", 75000 },
+            { "Habanero", 125000 }
+        };
+
+        /// <summary>
+        /// Calculates total spiciness based on the ingredients.
+        /// </summary>
+        /// <param name="ingredients">Comma-separated list of pepper names</param>
+        /// <returns>Total spiciness in SHU</returns>
+        /// <example>GET: /api/ChiliPeppers?ingredients=Poblano,Cayenne,Thai</example>
+        [HttpGet]
+        public IActionResult CalculateSpiciness([FromQuery] string ingredients)
+        {
+            int totalSpiciness = 0;
+            var peppers = ingredients.Split(',');
+
+            foreach (var pepper in peppers)
+            {
+                if (pepperHeatUnits.TryGetValue(pepper.Trim(), out int heat))
+                {
+                    totalSpiciness += heat;
+                }
+            }
+
+            return Ok(totalSpiciness);
+        }
+    }
+}
